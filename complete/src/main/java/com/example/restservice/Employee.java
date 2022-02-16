0127ec55bbd480087f1946a40e4f4f2d5340ba3a
Ttshadow <zhengxtong@gmail.com>
2022-02-16 18:03:52 -0500

    update

---
+++ complete/src/main/java/com/example/restservice/Employee.java
@@ -0,0 +1,46 @@
+package com.example.restservice;
+
+public class Employee {
+    int id;
+    String name;
+    String jobTitle;
+
+    public Employee(int id, String name, String jobTitle) {
+        this.id = id;
+        this.name = name;
+        this.jobTitle = jobTitle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
+}
--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -2,9 +2,7 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
@@ -16,4 +14,41 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@GetMapping("/test")
+	public String test(){
+		return "Hello Student";
+	}
+
+	@GetMapping("/person")
+	//	http://localhost:8081/person?fName=rose&lName=jack
+	public Person person(@RequestParam(value = "fName") String fName,
+						 @RequestParam(value = "lName") String lName){
+
+		return new Person(fName,lName);
+	}
+
+	@PostMapping("/person")
+	//	http://localhost:8081/person
+	public String createPerson(@RequestBody Person person){
+		System.out.println(person);
+		return "person is created";
+	}
+
+	@GetMapping("/employee")
+	public Employee employee(@RequestParam(value = "idNumber") int id,
+							 @RequestParam(value = "name") String name,
+							 @RequestParam(value = "job") String jobTitle){
+		return new Employee(id,name,jobTitle);
+	}
+
+	@PostMapping("/employee")
+	public String createEmployee(@RequestBody Employee employee){
+		System.out.println(employee);
+		return "employee is created";
+	}
+
+
+
+
 }
